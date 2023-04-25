a0dfd5c341e9f6522586a720ef5a859c79dba329
Rafael Machado <dev.rafaelmachado@gmail.com>
2023-04-24 22:40:05 -0300

    Test 1 (Create a hook)

--- app/src/pages/Dashboard.js
+++ app/src/pages/Dashboard.js
@@ -1,12 +1,14 @@
 import Style from "../css/pages/dashboard.module.css";
 
 import useAuth from "../hooks/useAuth";
+import useFirebaseValue from "../hooks/useFirebaseValue";
 
 import Hallo from "../components/Hallo";
 import Menu from "../components/Menu";
 
 const Dashboard = () => {
   const { logout } = useAuth();
+  const value = useFirebaseValue("room/components/bme280/humd/value");
 
   return (
     <div className={Style.dashboard}>
@@ -14,6 +16,7 @@ const Dashboard = () => {
         <Hallo />
         <Menu logout={logout} />
       </header>
+      <div>Teste: {value} </div>
     </div>
   );
 };
