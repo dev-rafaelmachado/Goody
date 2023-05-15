dac3bf4898247bf7e20d2895d16356fc21ea5014
Rafael Machado <dev.rafaelmachado@gmail.com>
2023-05-14 22:46:05 -0300

    done

--- app/src/components/modules/IPCam.js
+++ app/src/components/modules/IPCam.js
@@ -2,8 +2,14 @@ import Style from "../../css/components/ipcam.module.css";
 import { Webcam } from "@phosphor-icons/react";
 
 const Ipcam = () => {
+  const openApp = () => {
+    const intentURI =
+      "intent://#Intent;scheme=br.com.intelbras.mibocam;package=br.com.intelbras.mibocam;end;";
+    window.location.href = intentURI;
+  };
+
   return (
-    <div className={Style.box}>
+    <div onClick={openApp} className={Style.box}>
       <Webcam size={"3rem"} color="#fcfcfc" weight="fill" />
       <h2>IP CAM</h2>
     </div>
